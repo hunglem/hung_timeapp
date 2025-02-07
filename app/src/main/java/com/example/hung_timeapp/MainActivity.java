7abd782d7d4facdf11148d312f86e82ee9d79e50
hunglem <hungle02042005@gmail.com>
2025-02-07 21:18:12 +0700

    commit

--- app/src/main/java/com/example/hung_timeapp/MainActivity.java
+++ app/src/main/java/com/example/hung_timeapp/MainActivity.java
@@ -1,71 +1,154 @@
 package com.example.hung_timeapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import static android.app.PendingIntent.FLAG_IMMUTABLE;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
-    TextView currenTime, currenDate;
-    Handler handler;
-    SimpleDateFormat timeFormat;
-    SimpleDateFormat dateFormat;
-    public ImageButton imageButton;    
-    
-    
+    EditText timeHour;
+    EditText timeMinute;
+    Button setTime;
+    Button setAlarm;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currenTime = findViewById(R.id.timeTextView);
-        currenDate = findViewById(R.id.dateTextView);
-        
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
 
-        timeFormat = new SimpleDateFormat("HH:mm:ss");
-        dateFormat = new SimpleDateFormat("dd:MM:yyyy") ;
+        timeHour = findViewById(R.id.etHour);
+        timeMinute = findViewById(R.id.etMinute);
+        setTime = findViewById(R.id.btnTime);
+        setAlarm = findViewById(R.id.btnAlarm);
 
-        currenDate.setText(dateFormat.format(new Date()));
-        fab.setOnClickListener(view -> {
-            Intent iAlam = new Intent(MainActivity.this, SetAlarm.class);
+        Switch simpleSwitch1 = (Switch) findViewById(R.id.simpleSwitch1);
+        Button cancelalarm = (Button) findViewById(R.id.cancelalarm);
+        String statusSwitch1 = sp.getString("statusSwitch1", "1");
+
+        SharedPreferences.Editor editor = sp.edit();
+
+        if (statusSwitch1.equals("1")) {
+            simpleSwitch1.setChecked(true);
+        }
+        if (statusSwitch1.equals("0")) {
+            simpleSwitch1.setChecked(false);
+        }
+
+        simpleSwitch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (simpleSwitch1.isChecked()) {
+                    editor.putString("statusSwitch1", "1");
+                    editor.apply();
+                }
+                if (!simpleSwitch1.isChecked()) {
+                    editor.putString("statusSwitch1", "0");
+                    editor.apply();
+                }
+            }
         });
 
-        handler = new Handler();
-        updateCurentTime();
+        setTime.setOnClickListener((v) -> {
+            calendar = Calendar.getInstance();
+            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+            currentMinute = calendar.get(Calendar.MINUTE);
+            timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                    timeHour.setText(String.format("%02d", hourOfDay));
+                    timeMinute.setText(String.format("%02d", minutes));
+                }
+            }, currentHour, currentMinute, false);
 
-        imageButton = (ImageButton) findViewById(R.id.imageView);
+            timePickerDialog.show();
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        });
+
+        setAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,SetAlarm.class);
-                startActivity(intent);
+                String statusSwitch1 = sp.getString("statusSwitch1", "1");
+                if (statusSwitch1.equals("1")) {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("mm");
+                    String currenttimeMinute = dateFormat.format(new Date());
+                    SimpleDateFormat dateFormat2 = new SimpleDateFormat("hh");
+                    String currenttimeHour = dateFormat2.format(new Date());
+                    int hrdiff = Integer.parseInt(timeHour.getText().toString()) - Integer.parseInt(currenttimeHour);
+                    int hrinms = hrdiff * 3600000;
+                    int mindiff = Integer.parseInt(timeMinute.getText().toString()) - Integer.parseInt(currenttimeMinute);
+                    int mininms = mindiff * 60000;
+                    int totalms = hrinms + mininms;
+                    editor.putInt("totalms", totalms);
+                    editor.apply();
+                    startAlarm();
+                }
+
+                if (!timeHour.getText().toString().isEmpty() && !timeMinute.getText().toString().isEmpty()) {
+                    Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                    intent.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(timeHour.getText().toString()));
+                    intent.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(timeMinute.getText().toString()));
+                    intent.putExtra(AlarmClock.EXTRA_MESSAGE, "Alarm Notifcation");
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "There is no app that support this action", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Please choose a time", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cancelalarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
             }
         });
     }
 
-        private void updateCurentTime() {
-            currenTime.setText(timeFormat.format(new Date()));
-            handler.postDelayed(this::updateCurentTime,1000);
+    private void startAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent,FLAG_IMMUTABLE);
+        Integer totalms = sp.getInt("totalms", 0);
+        Integer seconds = sp.getInt("totalms", 0)/1000;
+        Toast.makeText(MainActivity.this, "Notifcation and Music will ring in " + seconds + "s" , Toast.LENGTH_SHORT).show();
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+totalms, pendingIntent);
     }
 
-        @Override
-        protected void onDestroy(){
-            super.onDestroy();
-            handler.removeCallbacksAndMessages(null);
-
-
-
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, FLAG_IMMUTABLE);
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(MainActivity.this, "Nofication and Music Alarm cancelled", Toast.LENGTH_SHORT).show();
     }
-
 }
